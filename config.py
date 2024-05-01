01e393bda275926dfdf6aec0d80cfd4d010270af
Kovalchuk <sashakovalchuk1955@gmail.com>
2024-05-01 10:52:19 +0300

    console exe run fix

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
 # файл для читання config.ini
-
 from configparser import ConfigParser
+import os
+import sys
 
 config = ConfigParser()
+
+# у разі запуску exe з консолі файл config.ini не знаходиться
+# дана стрічка коду вирішує цю проблему
+os.chdir(os.path.dirname(sys.argv[0]))
+# читання файлу config.ini
 config.read("config.ini")
 
 TOKEN = config["BOT"]["Token"]
